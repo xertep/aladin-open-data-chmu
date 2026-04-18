651973b884c20de511aa6725046b2fddfbbb48f6
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-04-18 22:30:05 +0200

    Add files via upload

--- precip_streamlit.py
+++ precip_streamlit.py
@@ -1029,6 +1029,20 @@ if layer == "Vítr" and "wind_speed_path" in st.session_state:
 
         ax.set_title("")
 
+        # --- WHITE LEGEND BACKGROUND ---
+        legend_box = mpatches.FancyBboxPatch(
+            (0.78, 0.62),   # x, y (bottom-left in axes coords)
+            0.22, 0.38,     # width, height
+            transform=ax.transAxes,
+            boxstyle="round,pad=0.02",
+            facecolor="white",
+            edgecolor="black",
+            linewidth=0.8,
+            alpha=0.9,
+            zorder=0
+        )
+
+        ax.add_patch(legend_box)
 
         # ---- WIND ARROWS (QUIVER) ----
         q = ax.quiver(
@@ -1050,20 +1064,6 @@ if layer == "Vítr" and "wind_speed_path" in st.session_state:
         ax.quiverkey(q, 0.9, 0.74, 10, "10 m/s", labelpos="E")
         ax.quiverkey(q, 0.9, 0.67, 13, "13 m/s", labelpos="E")
 
-        # --- WHITE LEGEND BACKGROUND ---
-        legend_box = mpatches.FancyBboxPatch(
-            (0.78, 0.62),   # x, y (bottom-left in axes coords)
-            0.22, 0.38,     # width, height
-            transform=ax.transAxes,
-            boxstyle="round,pad=0.02",
-            facecolor="white",
-            edgecolor="black",
-            linewidth=0.8,
-            alpha=0.9,
-            zorder=5
-        )
-
-        ax.add_patch(legend_box)
 
         # ---- MAP FEATURES ----
         ax.add_feature(cfeature.BORDERS, edgecolor="magenta", linewidth=1.2)
