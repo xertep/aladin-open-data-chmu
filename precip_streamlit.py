6a5ec335d63c416423a49438130166bbf590ee06
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-04-19 20:10:50 +0200

    Add files via upload

--- precip_streamlit.py
+++ precip_streamlit.py
@@ -300,7 +300,7 @@ czech_days_normal = [
 @st.cache_data(show_spinner=False)
 def load_kraje():
     gdf = gpd.read_file("kraje_wgs84.geojson")
-    gdf["geometry"] = gdf.geometry.simplify(0.03, preserve_topology=True)
+    gdf["geometry"] = gdf.geometry.simplify(0.02, preserve_topology=True)
     return gdf.geometry
 
 
